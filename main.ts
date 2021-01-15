4cdbe5d9acc8bb7e11e9e74841d4e48674d028e7
Tian134 <77447296+Tian134@users.noreply.github.com>
2021-01-15 19:55:35 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,13 +6,13 @@ basic.forever(function () {
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, speed)
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, speed)
         } else {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, speed)
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, turnspeed)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, turnspeed)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, speed)
         }
     } else {
         if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0) {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, turnspeed)
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, speed)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, turnspeed)
         } else {
             maqueen.motorStop(maqueen.Motors.All)
         }
